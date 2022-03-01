01d5d58758b4a337cc8ef396fb84faa1600c3244
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-02 00:27:21 +0700

    Change CSS for LinksIcon

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -5,7 +5,7 @@
 
 <a
   {href}
-  class="flex mx-8 xs:mx-0 justify-center items-center gap-2 transition ease-in-out border-2 px-4 py-2 rounded border-slate-700 dark:border-slate-200 text-slate-800 hover:bg-slate-200 dark:text-slate-400 dark:hover:bg-slate-800"
+  class="flex mx-0 sm:mx-4 my-2 sm:my-0 justify-center items-center gap-2 transition ease-in-out border-2 px-4 py-2 rounded border-slate-700 dark:border-slate-200 text-slate-800 hover:bg-slate-200 dark:text-slate-400 dark:hover:bg-slate-800"
 >
   {#if icon}<span>{icon}</span>{/if}
   <slot />
