56fccc1388a1270a139264fc1a6514cfd0338497
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-01 21:57:36 +0700

    Remove Kofi & Social Plugin

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import '../app.css'
   import Head from '$lib/components/Head.svelte'
-  import Kofi from '$lib/components/Kofi.svelte'
   import Menu from '$lib/components/Menu.svelte'
-  import Social from '$lib/components/Social.svelte'
   import ThemeSelector from '$lib/components/ThemeSelector.svelte'
   import Modal from '$lib/components/Modal.svelte'
   import { darkTheme, modalViewed } from '$lib/store'
@@ -21,8 +19,6 @@
 </script>
 
 <Head {title} {description} {url} {imageUrl} {gtagId} />
-<Kofi name="bossoq" label="Buy me Coffee" />
-<Social {url} {title} />
 <main class={$darkTheme && 'dark'}>
   <ThemeSelector {themeIcons} />
   <Menu items={menuItems} />
