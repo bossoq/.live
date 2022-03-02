1f074c2e38eda5c5e1e366f2c5d6638f7506f614
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-02 23:49:27 +0700

    Add projects to page

---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  interface Links {
+    name: string
+    demo?: string
+    github: string
+  }
+  export let links: Links[] = []
+</script>
+
+<div class="w-fit sm:w-3/4 flex flex-col sm:flex-row flex-wrap justify-center">
+  {#each links as { name, demo, github }}
+    <div
+      class="flex flex-col mx-0 sm:mx-4 my-2 sm:my-4 justify-center items-center gap-2 border-2 px-8 py-6 rounded-lg border-cyan-700 dark:border-cyan-200"
+    >
+      <h3 class="text-base sm:text-xl font-medium text-cyan-800 dark:text-cyan-200">{name}</h3>
+      {#if demo}
+        <a
+          href={demo}
+          target="_blank"
+          rel="noreferrer"
+          class="underline text-blue-800 dark:text-blue-200">[Live Demo]</a
+        >
+      {/if}
+      <a
+        href={github}
+        target="_blank"
+        rel="noreferrer"
+        class="underline text-blue-800 dark:text-blue-200">[GitHub]</a
+      >
+    </div>
+  {/each}
+</div>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,9 +1,42 @@
 <script lang="ts">
+  import Projects from '$lib/components/Projects.svelte'
   import BackToHome from '$lib/components/BackToHome.svelte'
+
+  const projectsList = [
+    {
+      name: 'สร้าง คำ เสี่ยว',
+      demo: 'https://siewword.bossoq.live',
+      github: 'https://github.com/bossoq/siew-generator'
+    },
+    {
+      name: 'SET Market Return Calculator',
+      demo: 'https://mktreturn.bossoq.live',
+      github: 'https://github.com/bossoq/MarketStatisticsReact'
+    },
+    {
+      name: 'Supapanya Tutoring School',
+      demo: 'https://supapanya.com',
+      github: 'https://github.com/bossoq/supapanya'
+    },
+    {
+      name: 'Sniffslive Twitch Bot',
+      github: 'https://github.com/Ponny035/SniffsLiveTwitchBot'
+    },
+    {
+      name: 'Sniffslive Discord Bot',
+      github: 'https://github.com/bossoq/SniffsbotDiscord'
+    },
+    {
+      name: 'Sniffslive Webfeed',
+      github: 'https://github.com/bossoq/SniffsbotWebfeed'
+    }
+  ]
 </script>
 
 <div class="w-full h-screen flex flex-col justify-center items-center bg-white dark:bg-black">
-  <h1 class="text-3xl sm:text-6xl flex flex-row text-black dark:text-white">PlaceHolder</h1>
+  <h1 class="text-3xl sm:text-6xl flex flex-row text-teal-800 dark:text-teal-200">Projects</h1>
+  <span class="my-4" />
+  <Projects links={projectsList} />
   <span class="my-4" />
   <BackToHome />
 </div>
